331cdf4e535f34a01f9a50bc1f22a536c4bab801
Ethan <ethancao@y7mail.com>
2023-02-13 12:47:43 +1100

    Fixed header, changed colour scheme, fixed up folders, implementation of pomodoro section

--- pomo_script.js
+++ pomo_script.js
@@ -1,8 +1,9 @@
-const semicircles = document.querySelectorAll('.semicirle');
+const semicircles = document.querySelectorAll('.semicircle');
+const timer = document.querySelector('.timer');
 
 // input
 const hr = 0;
-const min = 1;
+const min = 45;
 const sec = 0;
 
 // convert to milliseconds
@@ -13,6 +14,7 @@ const setTime = hours + minutes + seconds;
 const startTime = Date.now();
 const futureTime = startTime + setTime;
 
+// to run the function repetively
 const timerLoop = setInterval(countDownTimer);
 countDownTimer();
 
@@ -24,38 +26,38 @@ function countDownTimer() {
     // progress indicator
     if (angle > 180) {
         semicircles[2].style.display = 'none';
-        semicircles[0].style.transform = 'rotate(180deg)';
-        semicircles[1].style.transform = 'rotate(${angle}deg)';
+        semicircles[0].style.transform = `rotate(180deg)`;
+        semicircles[1].style.transform = `rotate(${angle}deg)`;
     } else {
         semicircles[2].style.display = 'block';
-        semicircles[0].style.transform = 'rotate($(angle)deg)';
-        semicircles[1].style.transform = 'rotate($(angle)deg)';
+        semicircles[0].style.transform = `rotate(${angle}deg)`;
+        semicircles[1].style.transform = `rotate(${angle}deg)`;
     }
 
     // timer
-    const hrs = Math.floor((remainingTime / (1000 * 60 * 60)) % 24);
-    const mins = Math.floor((remainingTime / (1000 * 60)) % 60);
-    const secs = Math.floor((remainingTime / 1000) % 60);
+    const hrs = Math.floor((remainingTime / (1000 * 60 * 60)) % 24).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
+    const mins = Math.floor((remainingTime / (1000 * 60)) % 60).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
+    const secs = Math.floor((remainingTime / 1000) % 60).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
     
-    timerLoop.innerHTML = `
+    timer.innerHTML = `
     <div>${hrs}</div>
     <div class="colon">:</div>
     <div>${mins}</div>
-    <div>:<div>
+    <div class="colon">:</div>
     <div>${secs}</div>
     `;
 
     // end
     if (remainingTime < 0) {
-        clearInterval(timerLoop)
+        clearInterval(timerLoop);
         semicircles[0].style.display = 'none';
         semicircles[1].style.display = 'none';
         semicircles[2].style.display = 'none';
 
-        timerLoop.innerHTML = `
+        timer.innerHTML = `
         <div>00</div>
         <div class="colon">:</div>
-        <div>00}</div>
+        <div>00</div>
         <div>:<div>
         <div>00</div>
         `;
